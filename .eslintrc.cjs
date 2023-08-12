fb023a581bb543920955fa6a694fb78cdd005692
jungwoo <jw.roh629@gmail.com>
2023-08-12 16:51:00 +0900

    eslint 추가

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,51 @@
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
-}
+	env: {
+		browser: true,
+		commonjs: true,
+		es2021: true,
+	},
+	extends: [
+		'xo',
+		'plugin:react/recommended',
+		'plugin:@typescript-eslint/recommended',
+	],
+	overrides: [
+		{
+			env: {
+				node: true,
+			},
+			files: [
+				'.eslintrc.{js,cjs}',
+			],
+			parserOptions: {
+				sourceType: 'script',
+			},
+		},
+		{
+			extends: [
+				'xo-typescript',
+			],
+			files: [
+				'*.ts',
+				'*.tsx',
+			],
+		},
+	],
+	parserOptions: {
+		ecmaVersion: 'latest',
+	},
+	plugins: [
+		'react',
+	],
+	"rules": {
+		'react-refresh/only-export-components': [
+			'warn',
+			{ allowConstantExport: true },
+		  ],
+		"quotes": ["error", "single"],
+		"no-duplicate-imports": "error",
+		"no-console": ["warn", { "allow": ["warn", "error", "info"] }],
+		"no-unused-vars": "error",
+		"no-multiple-empty-lines": "error"
+	  },
+};
